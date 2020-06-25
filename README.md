789eb555faf380563e7eef9ad99ff206e28ffdf1
roeiad <roeiadlersh@gmail.com>
2020-06-25 14:39:43 +0300

    added dadJokes.js

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # puzzlebot
+[![Downloads](https://img.shields.io/github/downloads/roeiad/puzzlebot/total.svg)](https://github.com/roeiad/puzzlebot/releases/latest)
+[![Stars](https://img.shields.io/github/stars/roeiad/puzzlebot?style=social)](https://github.com/roeiad/puzzlebot/stargazers)
+[![Release](https://img.shields.io/github/v/release/roeiad/puzzlebot)](https://github.com/roeiad/puzzlebot/releases/latest)
+
 base on [DiscordDadBot](https://github.com/willmac321/DiscordDadBot) by [willmac321](https://github.com/willmac321) with added puns
+
+| Command       | Description                                                                                                              
+| ------------- | ------------------------------------------------------------------------------------------------------------------------- 
+| p!help        | shows a list of all the commands      
+| p!start       | start the bot                                                                                           
+| p!puns        | return a random pun                                                                                           
+| p!dadjokes       | return a random dad joke                                                                
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 ﻿const Discord = require("discord.io")
 let logger = require('winston');
 let respond = true;
-let dadjokes = require('./commands/dadJokes')
+let dadJokes = require('./commands/dadJokes')
 let puns = require("./commands/puns")
-let botUser = "puzzle-bot"
-let prefix="p!"
+let text = require("./text.json")
+let {prefix,botUser}=require("./conf.json")
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -25,7 +25,6 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
 
     if (message.toLowerCase().indexOf("i'm " || "im") > -1 && user !== botUser && respond) {
-        //Classic Dad
         let i = message.toLowerCase().indexOf("i'm ");
         let myStr = (message.substring(i + 4)).split(" ");
         i = 0;
@@ -35,31 +34,28 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         let str = myStr[i];
         bot.sendMessage({
             to: channelID,
-            message: "Hi " + str + ", I'm puzzlebot!  Pleased to meat you, snot a problem!"
+            message: "Hi " + str + text.im
         });
-    } else if (message.toLowerCase().indexOf(prefix+"dadjoke") > -1 && user !== botUser && respond) {
-        bot.sendMessage({to: channelID, message:dadjokes.getADadJoke()});
-    }
-    else if (message.toLowerCase().indexOf(prefix+"puns") > -1 && user !== botUser && respond) {
-          bot.sendMessage({to: channelID, message: puns.getAPun()});
-    }
-    else if (message.toLowerCase().indexOf(prefix+"stop") > -1 && user !== botUser && respond) {
+    } else if (message.toLowerCase().indexOf(prefix + "dadjoke") > -1 && user !== botUser && respond) {
+        bot.sendMessage({to: channelID, message: dadJokes.getADadJoke()});
+    } else if (message.toLowerCase().indexOf(prefix + "puns") > -1 && user !== botUser && respond) {
+        bot.sendMessage({to: channelID, message: puns.getAPun()});
+    } else if (message.toLowerCase().indexOf(prefix + "stop") > -1 && user !== botUser && respond) {
         bot.sendMessage({
             to: channelID,
-            message: "There is not stopping puzzletron.  Just kidding.  \nType '!puzzlebotstart' to start me again."
+            message: text.stop
         });
         respond = false;
-    } else if (message.toLowerCase().indexOf(prefix+"start") > -1 && user !== botUser && !respond) {
+    } else if (message.toLowerCase().indexOf(prefix + "start") > -1 && user !== botUser && !respond) {
         bot.sendMessage({
             to: channelID,
-            message: "You've got to be joking me.\nType '!puzzlebotStop' to stop dad facts. \n\n" + getDadJoke()
+            message: text.start
         });
         respond = true;
-    }
-    else if (message.toLowerCase().indexOf(prefix+"help") > -1 && user !== botUser && respond) {
+    } else if (message.toLowerCase().indexOf(prefix + "help") > -1 && user !== botUser && respond) {
         bot.sendMessage({
             to: channelID,
-            message: "Beeperuski Booperino I am the grand puzzleBotino!  Your fish is my commandidoodily. \n commands: \n 'p!dadjokes' for a random dad joke  \n 'p!puns' for a random pun \n 'p!stop' to stop \n 'p!Start' to turn back on"
+            message: text.help
         });
     }
 
