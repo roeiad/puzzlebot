87373de72bdf637b487d41745da32cab96b7dbb9
roeiad <roeiadlersh@gmail.com>
2020-08-26 10:53:33 +0300

    updated index.js

--- events/message.js
+++ events/message.js
@@ -41,17 +41,17 @@ module.exports = async (client, message) => {
   if (cmd && !message.guild && cmd.conf.guildOnly)
     return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
 
-  if (message.content.startsWith("i'm ")){
-    let i = message.toLowerCase().indexOf("i'm" );
-    let myStr = (message.substring(i + 4)).split(" ");
-    i = 0;
-    while (myStr[i] === "" || myStr[i] === "a" || myStr[i] === "the" || myStr[i] === "an" || myStr[i] === "The" || myStr[i] === "An" || myStr[i] === "A") {
-      i++;
-    }
-    let str = myStr[i];
-     await message.channel.send("Hi " + str + text.im)
-
-  }
+  // if (message.content.startsWith("i'm ")){
+  //   let i = message.toLowerCase().indexOf("i'm" );
+  //   let myStr = (message.substring(i + 4)).split(" ");
+  //   i = 0;
+  //   while (myStr[i] === "" || myStr[i] === "a" || myStr[i] === "the" || myStr[i] === "an" || myStr[i] === "The" || myStr[i] === "An" || myStr[i] === "A") {
+  //     i++;
+  //   }
+  //   let str = myStr[i];
+  //    await message.channel.send("Hi " + str + text.im)
+  //
+  // }
 
   
   message.flags = [];
