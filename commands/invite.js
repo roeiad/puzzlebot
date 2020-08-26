8c2facd8243b5b163e0f835ae281c5d87fbd2e65
roeiad <roeiadlersh@gmail.com>
2020-08-26 12:23:07 +0300

    updated index.js

---
+++ commands/invite.js
@@ -0,0 +1,18 @@
+const text = require("../assets/text.json");
+exports.run = async (client, message) => {
+    message.author.send(text.invite);
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["bot"],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "invite",
+    usage:"puzzle!invite",
+    category: "commands",
+    description: text.help.invite,
+};
